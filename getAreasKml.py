1ccd87ee1faea61b14185941b4fa7cfe40e953fd
DanMeisler <danmsohem@gmail.com>
2016-11-17 20:15:23 +0200

    ...

--- getAreasKml.py
+++ getAreasKml.py
@@ -1,10 +1,14 @@
-from fastkml import kml
+import sys
+err = sys.stderr
+sys.stderr = open('nul', 'w')  # to silent lxml error (work on windows only)
+from fastkml import KML
+sys.stderr = err
 
 
 def getAreas(pathOfKml):
     # Parse the KML doc
     text = open(pathOfKml, encoding='utf-8').read()
-    k = kml.KML()
+    k = KML()
     k.from_string(text)
     document = list(k.features())[0]
     folder = list(document.features())[0]
@@ -12,7 +16,7 @@ def getAreas(pathOfKml):
         points = '{}'.format(p.geometry).replace(' 0.0', '').replace(', ', ',')[9:-2].split(',')
         poly = []
         for point in points:
-                poly.append((float(point.split(' ')[1]), float(point.split(' ')[0])))
+            poly.append((float(point.split(' ')[1]), float(point.split(' ')[0])))
         yield (p.name, poly)
 
 
