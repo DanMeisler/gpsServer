0170270531a1edf8b99c850fed6a9be1fe032c6c
DanMeisler <danmsohem@gmail.com>
2016-11-17 14:34:15 +0200

    read areas from kml...

---
+++ getAreasKml.py
@@ -0,0 +1,22 @@
+from fastkml import kml
+
+
+def getAreas(pathOfKml):
+    # Parse the KML doc
+    text = open(pathOfKml, encoding='utf-8').read()
+    k = kml.KML()
+    k.from_string(text)
+    document = list(k.features())[0]
+    folder = list(document.features())[0]
+    for p in folder.features():
+        points = '{}'.format(p.geometry).replace(' 0.0', '').replace(', ', ',')[9:-2].split(',')
+        poly = []
+        for point in points:
+                poly.append((float(point.split(' ')[1]), float(point.split(' ')[0])))
+        yield (p.name, poly)
+
+
+if __name__ == '__main__':
+    path = r'c:\Users\Meisler\Desktop\a.kml'
+    for area in getAreas(pathOfKml=path):
+        print(area)
