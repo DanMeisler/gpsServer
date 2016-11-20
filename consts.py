11f93318c39e7cbe6cdf4f8adcd6b38ca3248dc1
DanMeisler <danmsohem@gmail.com>
2016-11-20 20:05:32 +0200

    ...

--- consts.py
+++ consts.py
@@ -1,5 +1,5 @@
 mongodPath = r'C:\Program Files\MongoDB\Server\3.2\bin\mongod.exe'
-pathAreas = r'C:\Program Files (x86)\EasyPHP-Webserver-14.1b2\www\areas.kml'
+pathAreas = r'C:\Program Files (x86)\EasyPHP-Webserver-14.1b2\www\kml\areas.kml'
 attrNames = ['Modem ID', 'Unit ID', 'GPS_DATA', 'Bat voltage indicate', 'MCU_TEMPERATURE', 'Ex_TEMP_Sensor',
              'Latitude', 'Longitude', 'Area', 'Date']
 pathCurrentStateCsv = r'C:\Program Files (x86)\EasyPHP-Webserver-14.1b2\www\currentState.csv'
